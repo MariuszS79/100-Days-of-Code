aa27ed94a1f523e9774f8ad87cfe4ca052ffee3f
MariuszS79 <mariusz.soroko@gmail.com>
2021-11-22 17:58:22 +0000

    calculator

--- Day 10/Day 10.py
+++ Day 10/Day 10.py
@@ -1,3 +1,5 @@
+import os
+
 def operators():
     if operator=="+":
         return (first+next)
@@ -7,30 +9,36 @@ def operators():
         return (first*next)
     elif operator=="/":
         return (first/next)
-cont=""
-operator=""
-next=float(0)
-first=float(0)
 
 
-print("C A L C U L A T O R")
-first=float(input("What's the first number?: "))
-print("+\n-\n*\n/")
-score=0
+def program():
+    global operator
+    global next
+    global first
+    print("C A L C U L A T O R")
+    first=float(input("What's the first number?: "))
+    print("+\n-\n*\n/")
+    score=0
 
 
-count=True
-while count:
-    operator=input("Pick an operation: ")
-    next=float(input("What's the next number?: "))
-    score=operators()
-    print (first,operator,next,"=",score)
-    cont=input("Type 'y' to continue calculating with, or type 'n' to start a new calculation: ")
-    if cont =="n":
-        count=False
-        break
-    first=score
+    count=True
+    while count:
+        global cont
+        operator=input("Pick an operation: ")
+        next=float(input("What's the next number?: "))
+        score=operators()
+        print (first,operator,next,"=",score)
+        cont=str(input(f"Type 'y' to continue calculating with {score} or type 'n' to start a new calculation: "))
+        if cont =="n":
+            count=False
+            if os.name=="posix":
+                os.system('clear')
+            else:
+                os.system('cls')
+            program()
+                                                            
+        first=score
              
-
+program()
 
 
