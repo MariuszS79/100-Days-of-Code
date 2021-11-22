555bbce5b81faa1921e59b6badb594cc3e645f14
MariuszS79 <mariusz.soroko@gmail.com>
2021-11-22 13:01:41 +0000

    update1

--- Day 9/Day 9.py
+++ Day 9/Day 9.py
@@ -1,9 +1,34 @@
-from replit import clear
+import os
+dict={}
+
+def check_biggest_bid():
+  biggest_bid=max(dict, key=dict.get)
+  all_values=dict.values()
+  biggest_value=max(all_values)
+  print("The winner is ",biggest_bid, " with a bid of £", biggest_value)
+
 your_name=input("What is your name?: ")
 your_bid=input("What is your bid?: £")
+dict[your_name]=your_bid
 other_bidders=input("Are there any other bidders? Type 'yes' or 'no'.\n")
+if other_bidders!="yes":
+    check_biggest_bid()
 
 if other_bidders=="yes":
-    #print("\n"*200) #used as os.system was giving "TERM environment variable not set" on Thonny
-    clear()
-your_name=input("What is your name?: ")
+    if os.name=="posix":
+        os.system('clear')
+    else:
+        os.system('cls')
+
+while other_bidders=="yes":
+  your_name=input("What is your name?: ")
+  your_bid=input("What is your bid?: £")
+  dict[your_name]=your_bid
+  other_bidders=input("Are there any other bidders? Type 'yes' or 'no'.\n")
+  if other_bidders!="yes":
+    if os.name=="posix":
+          os.system('clear')
+    else:
+          os.system('cls')
+  if other_bidders!="yes":
+    check_biggest_bid()
