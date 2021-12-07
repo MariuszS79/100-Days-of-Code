016e5e25f2a7a8bbaf354487852f801a1cdc7a6f
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-12-07 12:13:33 +0000

    Update main.py

--- Intermediate/Day 16 - coffee machine OOP/main.py
+++ Intermediate/Day 16 - coffee machine OOP/main.py
@@ -1,3 +1,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+machine_is_on = True
+
+while machine_is_on:
+
+    what_coffee = input("What coffee would you like? (espresso/latte/cappuccino): ")
