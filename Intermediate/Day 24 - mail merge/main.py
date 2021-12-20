c1b39e4419f06762c166aa2fbd83b2f76edfab64
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-12-20 21:34:41 +0000

    1

--- Intermediate/Day 24 - mail merge/main.py
+++ Intermediate/Day 24 - mail merge/main.py
@@ -9,5 +9,3 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
 
-
-
