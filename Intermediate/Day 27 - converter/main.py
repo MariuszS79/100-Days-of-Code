bcd337800e31a5272046e29226c977638d7c4906
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-01-04 16:01:47 +0000

    Update main.py

---
+++ Intermediate/Day 27 - converter/main.py
@@ -0,0 +1,15 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("mile to km converter")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="text", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+window.mainloop()
