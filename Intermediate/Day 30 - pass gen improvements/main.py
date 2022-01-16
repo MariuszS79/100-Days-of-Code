0f2d6351d3a650f2916aaa4ea5c6a5fe5f268d68
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-01-16 20:54:17 +0000

    final

--- Intermediate/Day 30 - pass gen improvements/main.py
+++ Intermediate/Day 30 - pass gen improvements/main.py
@@ -58,6 +58,23 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -77,8 +94,8 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=40)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=28)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -86,6 +103,8 @@ email_entry.insert(0, "myemail@gmail.com")
 password_entry = Entry(width=28)
 password_entry.grid(row=3, column=1)
 
+search_button = Button(text="Search", width=7, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
