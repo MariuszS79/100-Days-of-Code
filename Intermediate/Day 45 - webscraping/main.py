f8b41326c2f88d5fe3932296b56cf17cc17d44ee
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-09 16:01:24 +0000

    1

--- Intermediate/Day 45 - webscraping/main.py
+++ Intermediate/Day 45 - webscraping/main.py
@@ -1,40 +1,10 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://news.ycombinator.com/news")
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-yc_web_page = (response.text)
-
-soup = BeautifulSoup(yc_web_page, "html.parser")
-
-anchor_tag = (soup.find_all(name="a", class_="titlelink"))
-
-
-linklist=[]
-
-for i in anchor_tag:
-    links = i.get("href")
-    linklist.append(links)
-
-print(*linklist, sep='\n')
+# Write your code below this line 👇
 
 
 
 
-
-
-
-
-
-
-# #import lxml
-#
-# with open("website.html", encoding="utf8") as file:
-#     contents = file.read()
-#
-# soup = BeautifulSoup(contents, "html.parser")
-#
-# #print(soup.find_all(name="a"))
-#
-# for tag in (soup.find_all(name="a")):
-#     print(tag.get("href"))
