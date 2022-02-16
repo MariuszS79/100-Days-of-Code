5d9607f6b4ac8d9df835ecf03229d070799c9f32
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-16 16:55:44 +0000

    1

--- Intermediate/Day 48 - Selenium/main.py
+++ Intermediate/Day 48 - Selenium/main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import selenium.webdriver.common.keys
-from threading import Timer
+import time
 
 path = r"C:\Users\Mariusz\Desktop\chromedriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=path)
@@ -9,15 +9,69 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element_by_id("cookie")
 
-def click():
-    cookie.click()
-    cookie.click()
-    cookie.click()
-    cookie.click()
-    cookie.click()
-    Timer(0.00001, click).start()
+check = time.time() + 5
+timeout = time.time() + 60*5
 
-click()
+cursor_price = 15
+grandma_price = 100
+factory_price = 500
+mine_price = 2000
+shipment_price = 7000
+alchemy_lab_price = 50000
+portal_price = 1000000
+time_machine_price = 123456789
+
+
+while time.time()<timeout:
+    money_web = driver.find_element_by_id("money")
+    money = int(money_web.text)
+    cookie.click()
+    if time.time() > check:
+        if money >= time_machine_price:
+            tier_8 = driver.find_element_by_id("buyTime machine")
+            tier_8.click()
+            time_machine_price*=1.1
+        elif money >= portal_price:
+            tier_7 = driver.find_element_by_id("buyPortal")
+            tier_7.click()
+            portal_price*=1.1
+        elif money >= alchemy_lab_price:
+            tier_6 = driver.find_element_by_id("buyAlchemy lab")
+            tier_6.click()
+            alchemy_lab_price*=1.1
+        elif money >= shipment_price:
+            tier_5 = driver.find_element_by_id("buyShipment")
+            tier_5.click()
+            shipment_price*=1.1
+        elif money >= mine_price:
+            tier_4 = driver.find_element_by_id("buyMine")
+            tier_4.click()
+            mine_price*=1.1
+        elif money >= factory_price:
+            tier_3 = driver.find_element_by_id("buyFactory")
+            tier_3.click()
+            factory_price*=1.1
+        elif money >= grandma_price:
+            tier_2 = driver.find_element_by_id("buyGrandma")
+            tier_2.click()
+            grandma_price *= 1.1
+        elif money >= cursor_price:
+            tier_1 = driver.find_element_by_id("buyCursor")
+            tier_1.click()
+            cursor_price*=1.1
+        check = time.time() + 5
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
