481d4a9d57ab3fad26db5591819da4a76891b7a2
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2022-02-17 16:33:52 +0000

    1

--- Intermediate/Day 48 - Selenium/main.py
+++ Intermediate/Day 48 - Selenium/main.py
@@ -22,37 +22,37 @@ portal_price = 1000000
 time_machine_price = 123456789
 
 
-while time.time()<timeout:
+while time.time() < timeout:
     money_web = driver.find_element_by_id("money")
-    money = (money_web.text)
-    money = money.replace(",","")
+    money = money_web.text
+    money = money.replace(",", "")
     money = int(money)
     cookie.click()
     if time.time() > check:
         if money >= time_machine_price:
             tier_8 = driver.find_element_by_id("buyTime machine")
             tier_8.click()
-            time_machine_price*=1.1
+            time_machine_price *= 1.1
         elif money >= portal_price:
             tier_7 = driver.find_element_by_id("buyPortal")
             tier_7.click()
-            portal_price*=1.1
+            portal_price *= 1.1
         elif money >= alchemy_lab_price:
             tier_6 = driver.find_element_by_id("buyAlchemy lab")
             tier_6.click()
-            alchemy_lab_price*=1.1
+            alchemy_lab_price *= 1.1
         elif money >= shipment_price:
             tier_5 = driver.find_element_by_id("buyShipment")
             tier_5.click()
-            shipment_price*=1.1
+            shipment_price *= 1.1
         elif money >= mine_price:
             tier_4 = driver.find_element_by_id("buyMine")
             tier_4.click()
-            mine_price*=1.1
+            mine_price *= 1.1
         elif money >= factory_price:
             tier_3 = driver.find_element_by_id("buyFactory")
             tier_3.click()
-            factory_price*=1.1
+            factory_price *= 1.1
         elif money >= grandma_price:
             tier_2 = driver.find_element_by_id("buyGrandma")
             tier_2.click()
@@ -60,33 +60,12 @@ while time.time()<timeout:
         elif money >= cursor_price:
             tier_1 = driver.find_element_by_id("buyCursor")
             tier_1.click()
-            cursor_price*=1.1
+            cursor_price *= 1.1
         check = time.time() + 5
 
-
+cps = driver.find_element_by_id("cps")
+cps = cps.text
+cps = ''.join(filter(str.isdigit, cps))
+print(f"your final score is {cps} cookies per second")
 
 driver.quit()
-
-
-
-
-
-
-
-
-# name = driver.find_element_by_class_name("top")
-# name.send_keys("Mariusz")
-#
-#
-# surname = driver.find_element_by_class_name("middle")
-# surname.send_keys("So")
-#
-# email = driver.find_element_by_class_name("bottom")
-# email.send_keys("m@o2.eu")
-#
-# signup = driver.find_element_by_class_name("btn")
-# signup.click()
-
-
-
-#driver.quit()
